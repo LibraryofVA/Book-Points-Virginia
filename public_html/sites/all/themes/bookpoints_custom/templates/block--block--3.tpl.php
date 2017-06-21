d80f9e8f6586e5022d488692c6a4fd978b1860ba
Jason Roma <jasonnroma@gmail.com>
2017-06-20 20:41:37 -0400

    Adding prorgram information to allow customization on a per program basis

--- public_html/sites/all/themes/bookpoints_custom/templates/block--block--3.tpl.php
+++ public_html/sites/all/themes/bookpoints_custom/templates/block--block--3.tpl.php
@@ -13,11 +13,13 @@ $entity=entity_load_single('reader', $current_reader);
 
 $current_reader_fname=$entity->field_first_name['und'][0]['value'];
 
+$program_id=".reader-program-" . $entity->field_program['und'][0]['target_id'];
+
   $form=drupal_get_form('reading_activity_form');
 
 ?>     
 
-<div id="book-log-entry-block">
+<div id="book-log-entry-block" class="<?php echo t($program_id)?>">
 
 <h2 class='centered-text'>Track Your Reading</h2>
 
